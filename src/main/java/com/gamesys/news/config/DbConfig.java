defa305d788e78acbeed27b122fbe7d5b6d5e65f
PEROOT\VDAYARA <raminda.dayananda@pearson.com>
2021-04-02 13:30:06 +0530

    add code coverage

--- src/main/java/com/gamesys/news/config/DbConfig.java
+++ src/main/java/com/gamesys/news/config/DbConfig.java
@@ -3,7 +3,6 @@ package com.gamesys.news.config;
 import com.gamesys.news.config.dtl.ConfigDtl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
